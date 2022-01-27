79ad4bbff21197553ea3a179dab210d592539e83
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-28 02:17:27 +0700

    update database

--- document/trang_web_ban_hang.sql
+++ document/trang_web_ban_hang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 07, 2022 at 04:37 PM
+-- Generation Time: Jan 27, 2022 at 07:14 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.19
 
@@ -68,7 +68,28 @@ CREATE TABLE `customers` (
 INSERT INTO `customers` (`id`, `name`, `gender`, `dob`, `email`, `phone`, `address`, `password`, `token`) VALUES
 (1, 'Hydra', 'male', '2021-12-15', 'longthanh@gmail.com', '', '', 'Long1234', ''),
 (2, 'hydra', 'male', '2000-09-02', 'longthanh1@gmail.com', '', '', 'Long1234', 'user_61c02f2f3092f9.427462291639984943'),
-(16, 'Bui huu Loc', 'Nam', '2022-01-01', 'feature451@gmail.com', '0123465798', 'Le lOi tuy Hoa', '123456', NULL);
+(16, 'Bui huu Loc', 'Nam', '2022-01-01', 'feature451@gmail.com', '0123465798', 'Le lOi tuy Hoa', '123456', NULL),
+(17, 'casca', 'Nam', '2021-12-28', 'cascas@csacsa', '4324234', 'cascsa', 'ca', NULL),
+(18, 'casca', 'Nam', '2022-01-05', 'casca@cas', '32432', 'cascsa', 'cas', NULL),
+(19, 'csaacasc', 'Nam', '2021-12-29', 'acavdfb@vsdvs', '2341421', 'cascsa', 'caca', NULL),
+(20, 'cascsa', 'Nam', '2022-01-03', 'ccsac@rgdfh', '42342432', 'sacas', 'csaca', NULL),
+(21, 'cascasc', 'Nam', '2021-12-29', 'sacascr32@facas', '43423', 'csa', 'csacas', NULL),
+(22, 'cascsa', 'Nam', '2021-12-29', 'cascsacc@vsdvds', '23123', 'cascsa', 'saca', NULL),
+(23, 'cascsacas', 'Nam', '2021-12-30', 'avdsvds@gf', '42342', 'csacsa', 'cas', NULL),
+(24, 'caascasca', 'Nam', '2022-06-07', 'avdsvsd@vdvs', '025621651', 'sdvdsvds', 'vdsvdsvds', NULL),
+(25, 'casa', 'Nam', '2021-12-28', 'scsa@csac', '4324234', 'cascsacsa', 'casca', NULL),
+(26, 'cascasc', 'Nam', '2022-01-21', 'acacacsaa@hh65y6', '4234234', 'cv', 'sdcdscsd', NULL),
+(27, 'cacsa', 'Nam', '2022-01-05', 'cascasca@vdvdsv', '32423432', 'ccascas', 'cacascascsa', NULL),
+(28, 'casc', 'Nam', '2022-01-04', 'ascascaca@vsdvs.com', '156156', 'dcsd', 'cdscsd', NULL),
+(29, 'casc', 'Nam', '2021-12-29', 'ascsaca@dvsvsd', '42423', 'casc', 'asas', NULL),
+(30, 'casc', 'Nam', '2022-01-05', 'cascsac@vdsvsd', '41414', 'cacas', 'csa', NULL),
+(31, 'câcscascascas', 'Nam', '2022-01-18', 'bfdbdf@bdfbdf.com', '5665615615', 'csacascascas', 'cdsavfsdv43', NULL),
+(32, '123cascas', 'Nam', '2021-12-29', 'vdsvs@vsvsd', '8681156', 'cascas', 'csacsa', NULL),
+(33, 'cascsac', 'Nam', '2021-12-29', 'ascas@cvdvsdvs', '23423423', 'sacas', 'casc', NULL),
+(34, 'cascsa', 'Nam', '2021-12-29', 'csacascas@12321312', '565156', 'cassc', 'acsa', NULL),
+(35, 'bdfbfdbdfb', 'Nam', '2021-12-29', 'fwvc@vsdvds', '265156', 'casc', 'ascsa', NULL),
+(36, 'cascqwfq', 'Nam', '2022-01-05', 'cascasc@vsdvsdv', 'r23423', 'cascs', 'acsa', NULL),
+(37, 'csaacssacsac', 'Nam', '2021-12-30', 'casccsa@cdcsa', '2134234213', 'csaca', 'ccsacsascacsacsa', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,12 +122,13 @@ CREATE TABLE `manufacturers` (
 --
 
 INSERT INTO `manufacturers` (`id`, `name`, `address`, `phone`, `image`) VALUES
-(3, 'Fissler', 'Đức', '0123456789', 'https://lh3.googleusercontent.com/proxy/o7ojTkGBn2LbPieb7TRVw6wHlnpHYCpXuABugZgbZBxMTX6XCwc3VRPoQkdEJ9UkiIlc6w1R79gJCelYZGPjGoydR42T-Nx35lsYCwMlYLlGnrMQ'),
+(3, 'Fisslerrrr', 'Đức', '0984123456', 'https://lh3.googleusercontent.com/proxy/o7ojTkGBn2LbPieb7TRVw6wHlnpHYCpXuABugZgbZBxMTX6XCwc3VRPoQkdEJ9UkiIlc6w1R79gJCelYZGPjGoydR42T-Nx35lsYCwMlYLlGnrMQ'),
 (4, 'Silit', 'Đức', '0987654321', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Silit-logo.svg/2560px-Silit-logo.svg.png'),
-(6, 'Minh Châu', 'Việt Nam', '0147258369', 'https://suminhchau.vn/wp-content/uploads/2019/11/su-minh-chau-fav-400x400.png'),
 (7, 'Bodoca', 'Việt Nam', '0369258147', 'https://thietbimiennam.com/wp-content/uploads/2016/10/logo-bodoca.png'),
 (8, 'Điện máy xanh', 'Việt Nam', '095184762', 'https://prices.vn/photos/7/store/ma-giam-gia-dienmayxanh.png'),
-(9, 'SunHouse', 'Việt Nam', '0159236478', 'https://upload.wikimedia.org/wikipedia/vi/e/ed/Logo_cong_ty_sunhouse.png');
+(9, 'SunHouse', 'Việt Nam', '0159236478', 'https://upload.wikimedia.org/wikipedia/vi/e/ed/Logo_cong_ty_sunhouse.png'),
+(10, 'casccsascacsasc', 'cascascscacsacsa', 'cascsaasccsaa', 'cascacascsacsacsacascsacas'),
+(11, 'cascbasjhcbas', 'cascsacascsacas', '0963852740', 'https://upload.wikimedia.org/wikipedia/commons/4/43/Latin_letter_A_with_acute.svg');
 
 -- --------------------------------------------------------
 
@@ -128,7 +150,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `manufacturer_id`) VALUES
-(2, 'Cây lau nhà DMX CL005 ', 'Cây lau nhà có thân bằng inox cứng cáp, chiều dài 128 cm dễ sử dụng.\r\nTay cầm bọc nhựa chống trơn trượt, thân cây có khóa chắc chắn, có móc treo cất giữ.\r\nBông lau nhà bằng sợi cotton bền chắc, thấm hút tốt, giặt rửa dễ dàng.\r\nThương hiệu DMX - độc quyền Điện máy XANH, sản xuất tại Việt Nam.', 40000, 'images/1639849341.jpg', 8),
+(2, 'Cây lau nhà DMX CL005555 ', 'Cây lau nhà có thân bằng inox cứng cáp, chiều dài 128 cm dễ sử dụng.\r\nTay cầm bọc nhựa chống trơn trượt, thân cây có khóa chắc chắn, có móc treo cất giữ.\r\nBông lau nhà bằng sợi cotton bền chắc, thấm hút tốt, giặt rửa dễ dàng.\r\nThương hiệu DMX - độc quyền Điện máy XANH, sản xuất tại Việt Nam.', 40000, 'images/1639849341.jpg', 8),
 (3, 'Cây lau nhà xoay 360 ĐỘ SUNHOUSE KS-MO350I', 'Chất liệu cao cấp, an toàn cho sức khỏe\r\nCán bằng inox 201 có tay cầm bọc nhựa PP chắc chắn\r\nBộ phận tạo chuyển động bằng thép và nhựa POM siêu bền\r\nBông lau bằng sợi Microfiber thấm hút nước tốt', 459000, 'images/1639849505.jpg', 9),
 (4, 'Nồi từ FISSLER PRO COLLECTION HIGH STOCK POT 28CM 14L', 'Nhập khẩu nguyên chiếc CHLB Đức - Made in Germany\r\nNồi từ Fissler làm từ vật liệu thép không gỉ chất lượng cao 18/10 dày, đặc, truyền nhiệt hiệu quả\r\nĐáy nồi Cookstar Allstove, nấu được trên mọi loại bếp, kể cả bếp từ, hạn chế cháy cục bộ, không cong vênh, lồi lõm\r\nLõi nhôm dày hoa lỏng ở 600ºC trước khi dập các lớp với nhau bởi một lực 1500 tấn\r\nĐường kính nồi 28cm\r\nTổng dung tích 14L\r\nTrọng lượng nồi 4.97Kg\r\nChiều cao nồi 24cm\r\nThước đo mực nước đến 12.5L\r\nVung inox thiết kế lõm lòng chảo giúp đối lưu hơi nước, chịu được 220ºC\r\nTay cầm cách nhiệt, thiết kế thẩm mỹ dạng đũa, cầm nắm thoải mái, chịu lực 150Kg\r\nMiệng rót chống tràn hiệu quả, cực khít với vung\r\nHiệu quả đun nấu nhanh, bảo toàn dinh dưỡng\r\nNồi nấu được trong lò nướng và vệ sinh an toàn với máy rửa bát.', 11500000, 'images/1639849625.jpg', 3),
 (5, 'Bộ nồi chảo Silit Pisa 10 Món', 'Nắp vung kính cường lực bền đẹp thuận tiện quan sát đồ ăn\r\nPhù hợp với mọi loại: bếp từ, bếp hồng ngoại…\r\nTỏa nhiệt đều, giữ nhiệt lâu\r\nAn toàn cho sức khỏe\r\nĐáy 3 lớp bắt từ nhanh giúp tiết kiệm điện\r\nXuất xứ: Nhập khẩu từ Đức', 6540000, 'images/1639849720.1000x1000', 4),
@@ -139,7 +161,41 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `manufact
 (10, 'Bơm Thông hút bồn cầu', 'Sản phẩm là 1 cái bơm\r\nDùng để Thông tắc bồn cầu\r\nKhi bồn cầu bị tắc bạn cần đổ 1 gói Thông cống vào để khoảng 1-2 tiếng xả nước rồi cho bơm vào bơm\r\nĐảm bảo hết tắc\r\nNếu vẫn tắc bạn cần đổ 2 chai CoCa cô la vào để qua đêm, rồi dùng bơm hút sẽ hết tắc', 80000, 'images/1639990223.jpg', 8),
 (11, 'Máy hút bụi không túi FC9351/01', 'Máy hút bụi không túi Philips Dòng 3000 với kích cỡ nhỏ gọn, sử dụng công nghệ PowerCyclone 5 và đầu hút MultiClean cho hiệu suất hút cao giúp bạn vệ sinh sạch sẽ mọi ngóc ngách bên trong nhà', 3499000, 'images/1639990283.jpg', 8),
 (12, 'Tủ 2 Cửa 1 Ngăn Kéo Modulo Home KAI1334-2', 'Tủ 2 Cửa 1 Ngăn Kéo Modulo Home KAI1334-2 được làm hoàn toàn bằng chất liệu gỗ công nghiệp cao cấp phủ giấy PU vân gỗ, không cong vênh, không co ngót, có độ bền cao và rất thân thiện với môi trường. Qua quá trình xử lý, bề mặt tủ có độ bóng đẹp và nhẵn mịn, các góc cạnh được gia công kỹ lưỡng để không gây trầy xước cho người dùng trong suốt quá trình sử dụng.\r\nTủ được thiết kế với 3 ngăn (trong đó có 01 ngăn kéo phía trên, 2 ngăn dưới kèm cửa đóng) tiện lợi để bạn có thể phân loại và sắp xếp đồ đạc phù hợp với nhiều mục đích sử dụng khác nhau. Bạn có thể sử dụng các ngăn tủ để cất giữ những bộ quần áo, lưu trữ sách, các loại băng đĩa hay đồ lưu niệm một cách gọn gàng và trang nhã. Mỗi ngăn kéo, cửa tủ đều có một tay cầm nhỏ gọn và chắc chắn, đóng mở rất chắc chắn và dễ dàng.\r\nTủ Modulo Home Kai - 1334-2 có màu nâu tự nhiên của gỗ mang đến vẻ đẹp sang trọng. Bạn có thể kết hợp tủ với bàn, ghế hay kệ cùng tông màu để không gian nội thất trong nhà thêm sang trọng, hiện đại.\r\nSản phẩm được giao hàng tháo rời, đóng gói trong thùng carton. Khách hàng lắp ráp sản phẩm tại nhà theo hướng dẫn lắp ráp kèm theo bao bì. Dịch vụ lắp ráp tại nhà có thu phí khu vực Tp. HCM, điện thoại 028 2223 7788', 1250000, 'images/1639990377.jpg', 7),
-(13, 'Đèn ngủ LED di động TaoTronics TT-DL23, pin 4000mAh, 110 giờ sử dụng, ánh sáng 360 độ', 'Đèn ngủ di động cao cấp ánh sáng 360 độ.\r\nThiết kế không dây an toàn và di động: Đèn sử dụng bộ điều khiển tích hợp sẵn để có thêm tính di động ở nhà, trong văn phòng và ngay cả khi không có ổ cắm điện xung quanh, như cắm trại và dã ngoại ngoài trời.\r\nPin 110 giờ: Được hưởng lợi từ pin sạc có thể sạc lại với dung lượng lên tới 4000mAh sẽ vượt xa nhu cầu và mong đợi của bạn - lên tới 110 giờ với ánh sáng luôn ở độ sáng tối thiểu.\r\nÁnh sáng 360 ° thân thiện với mắt: Đừng lo lắng về việc đánh thức người đang ngủ bên cạnh bạn (bao gồm cả em bé của bạn) Với bảng điều khiển 360 ° cung cấp tia sáng đồng đều và tinh tế hơn.\r\nĐiều chỉnh độ sáng: Đặt ngón tay của bạn trên bảng điều khiển cảm ứng và tùy chỉnh độ sáng. Tận hưởng nhiều lựa chọn hơn mà không cần phải cài đặt trước.\r\nĐiều khiển một chạm: Dễ dàng bật / tắt đèn bằng cách nhấn một lần bảng điều khiển cảm ứng được đặt ở phía trên cùng của đèn.\r\nSản phẩm chính hãng thương hiệu TaoTronics (Mỹ) bảo hành 12 tháng toàn quốc.', 390000, 'images/1639991046.jpg', 8);
+(13, 'Đèn ngủ LED di động TaoTronics TT-DL23, pin 4000mAh, 110 giờ sử dụng, ánh sáng 360 độ', 'Đèn ngủ di động cao cấp ánh sáng 360 độ.\r\nThiết kế không dây an toàn và di động: Đèn sử dụng bộ điều khiển tích hợp sẵn để có thêm tính di động ở nhà, trong văn phòng và ngay cả khi không có ổ cắm điện xung quanh, như cắm trại và dã ngoại ngoài trời.\r\nPin 110 giờ: Được hưởng lợi từ pin sạc có thể sạc lại với dung lượng lên tới 4000mAh sẽ vượt xa nhu cầu và mong đợi của bạn - lên tới 110 giờ với ánh sáng luôn ở độ sáng tối thiểu.\r\nÁnh sáng 360 ° thân thiện với mắt: Đừng lo lắng về việc đánh thức người đang ngủ bên cạnh bạn (bao gồm cả em bé của bạn) Với bảng điều khiển 360 ° cung cấp tia sáng đồng đều và tinh tế hơn.\r\nĐiều chỉnh độ sáng: Đặt ngón tay của bạn trên bảng điều khiển cảm ứng và tùy chỉnh độ sáng. Tận hưởng nhiều lựa chọn hơn mà không cần phải cài đặt trước.\r\nĐiều khiển một chạm: Dễ dàng bật / tắt đèn bằng cách nhấn một lần bảng điều khiển cảm ứng được đặt ở phía trên cùng của đèn.\r\nSản phẩm chính hãng thương hiệu TaoTronics (Mỹ) bảo hành 12 tháng toàn quốc.', 390000, 'images/1639991046.jpg', 8),
+(14, 'câcscascsacas', 'cấccascacsacsa', 16165, 'images/1642674086.png', 3),
+(15, 'San pham cuc ki oke', 'okeokeokeokeoeko', 50, 'images/1643209293.png', 8),
+(16, 'San pham keeee', '156161561651651', 155, 'images/1643209398.png', 3),
+(17, 'csaaaaaaaaaaaaaaaa', 'acassssssssssssss', 23, 'images/1643209555.jpg', 3),
+(18, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209706.png', 3),
+(19, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209719.png', 3),
+(20, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209735.png', 3),
+(21, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209781.png', 3),
+(22, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209952.png', 3),
+(23, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643209994.png', 3),
+(24, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643210040.png', 3),
+(25, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643210049.png', 3),
+(26, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643210061.png', 3),
+(27, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643210140.png', 3),
+(28, 'bbhjbjhbchjasbcjh', 'sdbhcjbajsdbhcjbaj', 50, 'images/1643210154.png', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_type`
+--
+
+CREATE TABLE `product_type` (
+  `product_id` int NOT NULL,
+  `type_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_type`
+--
+
+INSERT INTO `product_type` (`product_id`, `type_id`) VALUES
+(28, 1),
+(28, 2);
 
 -- --------------------------------------------------------
 
@@ -163,9 +219,9 @@ CREATE TABLE `receipts` (
 --
 
 INSERT INTO `receipts` (`id`, `customer_id`, `receiver_name`, `receiver_phone`, `receiver_address`, `status`, `order_time`, `total_price`) VALUES
-(8, 2, 'hydra ', '0123456789', 'Le Loi', 1, '2021-12-28 08:18:08', 43954000),
-(9, 2, 'Huu Loc', '0159478236', 'Nguyen Hue', 2, '2021-12-28 08:18:35', 5568000),
-(10, 2, 'Kim Hue', '0369852147', 'Gia Lai', 0, '2021-12-28 08:19:03', 23280000),
+(8, 2, 'hydra ', '0123456789', 'Le Loi', 6, '2022-01-24 11:53:52', 43954000),
+(9, 2, 'Huu Loc', '0159478236', 'Nguyen Hue', 6, '2022-02-02 08:18:35', 5568000),
+(10, 2, 'Kim Hue', '0369852147', 'Gia Lai', 6, '2021-12-28 08:19:03', 23280000),
 (11, 1, 'Huu Loc', '012346798', 'Le Loi', 0, '2021-12-29 09:53:36', 499000),
 (12, 1, 'Nguyen Ti', '015947826', 'Nguyen Thai Hoc', 0, '2021-12-29 09:53:58', 11899000),
 (13, 1, 'Ho Ngoc Han', '0065165165', 'Nguyen Thai Hoc', 1, '2021-12-29 09:56:34', 649000);
@@ -197,12 +253,31 @@ INSERT INTO `receipt_detail` (`receipt_id`, `product_id`, `quantity`) VALUES
 (9, 12, 1),
 (10, 2, 7),
 (10, 4, 2),
-(11, 2, 1),
+(11, 2, 10),
 (11, 3, 1),
 (12, 4, 1),
 (12, 7, 1),
 (13, 9, 1),
 (13, 10, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `name`) VALUES
+(1, 'nhà cửa'),
+(2, 'nấu ăn');
 
 --
 -- Indexes for dumped tables
@@ -243,6 +318,12 @@ ALTER TABLE `products`
   ADD KEY `manufacturer_id` (`manufacturer_id`);
 
 --
+-- Indexes for table `product_type`
+--
+ALTER TABLE `product_type`
+  ADD PRIMARY KEY (`product_id`,`type_id`);
+
+--
 -- Indexes for table `receipts`
 --
 ALTER TABLE `receipts`
@@ -258,6 +339,12 @@ ALTER TABLE `receipt_detail`
   ADD KEY `receipt_detail_ibfk_1` (`product_id`);
 
 --
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -271,25 +358,31 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `manufacturers`
 --
 ALTER TABLE `manufacturers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `receipts`
 --
 ALTER TABLE `receipts`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
